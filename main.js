aca60322ff626c8c97bb1ef3736fe646beca1f6e
gustavo-barrios2006 <gabarrios2006@gmail.com>
2024-07-01 13:45:53 -0300

    Adicionando lógica para adicionar e retirar a classe ativa quando o botão for pressionado através do teclado, e melhorando segurança da função tocarSom.

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 const listaDeTeclas=document.querySelectorAll(".tecla");
-function tocaSom(idElementoAudio)
+function tocaSom(seletorElementoAudio)
 {
-document.getElementById(idElementoAudio).play();
+let elemento=document.querySelector(seletorElementoAudio);
+if(elemento!&&elemento.localName==="button")
+{
+elemento.play();
+}
+else
+if(elemento.localName!=="button")
+{
+alert("Elemento inválido");
+}
+else
+{
+alert("seletor não encontrado");
+}
 }
 for(let contador=0;contador<listaDeTeclas.length;contador++)
 {
 listaDeTeclas[contador].onclick=function(){
-tocaSom(`som_${this.classList[1]}`);
+tocaSom(`#som_${this.classList[1]}`);
+};
+listaDeTeclas[contador].onkeydown=function(evento)
+{
+if(evento.code==="Enter"|evento.code==="Enter")
+{
+this.classList.add("ativa");
+}
+};
+listaDeTeclas[contador].onkeyup=function()
+{
+this.classList.remove("ativa");
 };
 }
